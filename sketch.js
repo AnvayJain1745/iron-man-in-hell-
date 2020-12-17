92bfbf6083dc66248088c18fddd13efb22c59b33
AnvayJain1745 <68576271+AnvayJain1745@users.noreply.github.com>
2020-12-17 18:12:23 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
+var lifeThanos,lifeIronMan
+var Laser,laser
 var box1
-var backGroundImage
+var backGroundImage,BACKGROUND
 var thanosImage,thanos
 var ironMan,ironManLeft,ironManRight,ironManFly
 var characterButtonImage,characterButton
@@ -15,6 +17,7 @@ function preload(){
   commentBoxImage2=loadImage("images/commentBox2.png")
   characterButtonImage=loadImage("images/characterButton.png")
   trainingButtonImage=loadImage("images/training.png")
+  laser=loadImage("images/laser 2.png")
 
 }
 function setup() {
@@ -54,42 +57,104 @@ function setup() {
   box1.addImage("box1",backGroundImage)
   box1.depth=2
   box1.visible=false
+
+  BACKGROUND=createSprite(displayWidth/2,displayHeight/2)
+  BACKGROUND.addImage("q",backGroundImage)
+  BACKGROUND.depth=0
+
+  lifeThanos=createSprite(thanos.x,thanos.y-170,100,20)
+  lifeThanos.shapeColor="red";
+
   
+  lifeIronMan=createSprite(ironMan.x,ironMan.y-170,100,20)
+  lifeIronMan.shapeColor="green";
+  console.log(ironMan.y)
 }
 
 function draw() {
-  background(backGroundImage);
-  console.log(ironMan.y)
+  
+  background(1,1,1,1);
+  Laser=createSprite(ironMan.x-150,ironMan.y-70)
+  Laser.addImage("laser",laser)
+  Laser.visible=false
+  Laser.scale=0.2
+  
+console.log(thanos.width+ "," +thanos.height)
+
+
+  
+
   if(mousePressedOver(characterButton)){
     
   }
-
+ /* if(keyDown("LEFT_ARROW") && ironMan.y<570){
+    ironMan.velocityX=-1
+    
+  }
+  if(keyDown("RIGHT_ARROW") && ironMan.y<570){
+    ironMan.velocityX=1
+        
+  }
+*/
+if(Laser.isTouching(thanos)){
+  lifeThanos.width=lifeThanos.width-5
+  console.log("1")
+       }
   if(keyDown("UP_ARROW")){
     ironMan.addImage("IMI",ironManFly)
     ironMan.velocityY=-1
-    
+    lifeIronMan.velocityY=-1
   }
   if(keyDown("LEFT_ARROW")){
-    ironMan.addImage("IMI",ironManLeft)
-    ironMan.velocityY=0
+  //  ironMan.addImage("IMI",ironManLeft)
+    ironMan.velocityX=-1
+    lifeIronMan.velocityX=-1
+
   }
   if(keyDown("RIGHT_ARROW")){
-    ironMan.addImage("IMI",ironManRight)
-    ironMan.velocityY=0
-  }
+    // ironMan.addImage("IMI",ironManRight)
+     ironMan.velocityX=1
+     lifeIronMan.velocityX=1
+     
+   }
+   if(keyDown("RIGHT_ARROW")){
+    // ironMan.addImage("IMI",ironManRight)
+     ironMan.velocityX=1
+     lifeIronMan.velocityX=1
+     
+   }
+   if(keyDown("DOWN_ARROW")){
+    // ironMan.addImage("IMI",ironManRight)
+     ironMan.velocityY=1
+     lifeIronMan.velocityY=1
+     
+   }
 
+   if(keyDown("space")){
+Laser.visible=true
+Laser.velocityX=-7
+ironMan.addImage("IMI",ironManLeft)
+   }
+     if(ironMan.y>440 && ironMan.y<450){
+      ironMan.velocityX=0
+      ironMan.velocityY=0
+      ironMan.addImage("IMI",ironManLeft)
+     }
+
+     
   if(comment1.lifetime===1){
     comment2.visible=true;
   }
   if(mousePressedOver(trainingButton) && comment2.lifetime<2 ){
     box1.visible=true
-    text("press up arrow key to make Iron Man fly")
-    text("press down arrow key to make Iron Man come to ground after flying")
-    text("press left arrow key to move Iron Man left")
-    text("press right arrow key to move Iron Man right")
+    text("press up arrow key to make Iron Man fly",200,300)
+    text("press down arrow key to make Iron Man come to ground after flying",250,300)
+    text("press left arrow key to move Iron Man left",300,300)
+    text("press right arrow key to move Iron Man right",350,300)
   }
   if(mousePressedOver(trainingButton) && box1.visible===true){
 
   }
  drawSprites()
+ 
 }
